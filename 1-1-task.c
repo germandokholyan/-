5069aaee1ceeec3cd4fca724d31927c70d45631e
germandokholyan <gerahautani@gmail.com>
2024-10-21 15:50:41 +0300

    Update 1-1-task.c

--- 1-1-task.c
+++ 1-1-task.c
@@ -8,7 +8,7 @@
  * @param z - значение параметра z
  * @return рассчитанное значение
  */
-double geta(const double x, const double y, const double z, const double sum, const double tg);
+double geta(const double x, const double y, const double z);
 
 /**
  * @brief рассчитывает выражение b
@@ -17,30 +17,28 @@ double geta(const double x, const double y, const double z, const double sum, co
  * @param z - значение параметра z
  * @return рассчитанное значение
  */
-double getb(const double x, const double y, const double z, const double sum, const double tg);
+double getb(const double x, const double y, const double z);
  
 /**
  * @brief точка входа в программу
  * @return 0,случае успеха
  */
-int main() 
+int main(void) 
 {
     const double x = 0.61, y = 3.4, z = 16.5;
-    const double sum = x + y;
-    const double tg = tan(pow(sum, 2));
     printf("x = %f", x);
     printf("y = %f", y);
     printf("z = %f\n", z);
-    printf("a = %f\n", geta(x, y, z, sum, tg));
-    printf("b = %f\n", getb(x, y, z, sum, tg));
+    printf("a = %f\n", geta(x, y, z));
+    printf("b = %f\n", getb(x, y, z));
     return 0;
 }
-double geta(const double x, const double y, const double z, const double sum, const double tg)
+double geta(const double x, const double y, const double z)
 {
-    return pow(x, 3) * pow(tg, 2) + (z / sqrt(sum));
+    return pow(x, 3) * pow(tan(pow(x + y, 2)), 2) + (z / sqrt(x + y));
 }
 
-double getb(const double x, const double y, const double z, const double sum, const double tg)
+double getb(const double x, const double y, const double z)
 {
     return (y * pow(x, 2) - z) / (exp(z * x) - 1);
 }
